6e9901a6c77aaa89bc44f58f38c827ca464e0364
Jasmine Perez <jasperez31@gmail.com>
2022-12-04 19:35:48 -0500

    create main page

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const constructorMethod = (app) => {
+  app.use('/', (req, res) => {
+    res.sendFile(path.resolve("src/static/about.html"));
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({error: 'Not found'});
+  });
+};
+
+module.exports = constructorMethod;
