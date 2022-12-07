9fffba4b44fb9ccd0b99309fc93f01897de21cdf
Campbell Tedtsen <ctedtsen@gmail.com>
2022-12-07 17:01:02 -0500

    fixed app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-var session = require('express-session')
 const app = express();
 const static = express.static(__dirname + '/public');
 const configRoutes = require('./routes');
@@ -12,13 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, '/views'));
-app.use(session({
-  name: 'AuthCookie',
-  secret: 'some secret string!',
-  resave: false,
-  saveUninitialized: true
-}))
 
 configRoutes(app);
 
