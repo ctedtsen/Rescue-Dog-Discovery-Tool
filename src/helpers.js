8d35468271e461ba3bee93f35a6989d89cca4425
Campbell Tedtsen <ctedtsen@gmail.com>
2022-12-07 23:22:50 -0500

    updated helpers and package.json

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 const { objectId } = require("mongodb");
+const bcrypt = require('bcryptjs');
 const fs = require("fs");
 const numbers = ["0", "1", "2,", "3", "4", "5", "6", "7", "8", "9"];
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
